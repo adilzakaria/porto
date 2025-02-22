d21614736753c6bc9a9c858237fdae29080af22e
Adil Zakaria <adilzakaria18@gmail.com>
2025-02-22 09:44:38 +0700

    3

--- components/home-page/home.tsx
+++ components/home-page/home.tsx
@@ -1,17 +1,10 @@
+import React from 'react'
+
 const Introduce = () => {
     return (
         <>
 		<div className="relative h-[320px] overflow-hidden">
 		<div className="absolute inset-0" />
-		{/* <div className="flex items-center justify-center">
-		<Image
-			src="/abe.jpg"
-			alt="Profile Picture"
-			width={300}
-			height={300}
-			className="rounded-full border-4 border-white shadow-lg"
-		/>
-		</div> */}
 		<div className="mt-16 text-center">
 			<h1 className="text-3xl md:text-5xl font-bold mb-6 text-gray-800">
 			HEY, I'M ADIL ZAKARIA
