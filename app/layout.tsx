546c32283407da2ff531d2ceefa53669e91464df
Adil Zakaria <adilzakaria18@gmail.com>
2025-02-22 10:44:33 +0700

    7

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,12 @@
 import "./globals.css";
 import Header from "@/components/home-page/navbar";
 import SocialNavbar from "@/components/home-page/social-navbar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Adil Zakar's Portfolio",
+  description: "My site description", 
+}
 
 export default function RootLayout({
   children,
